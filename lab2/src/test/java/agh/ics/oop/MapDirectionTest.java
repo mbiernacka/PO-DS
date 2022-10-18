d3a3e60afe764a58a3f762d569f93cbf24e62b36
Milena <milena.biernackaa@gmail.com>
2022-10-18 11:18:44 +0200

    Modyfikacja testów, ostatnie poprawki lab2

--- lab2/src/test/java/agh/ics/oop/MapDirectionTest.java
+++ lab2/src/test/java/agh/ics/oop/MapDirectionTest.java
@@ -6,37 +6,22 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MapDirectionTest {
 
     @Test
-    void NextForNorthIsEast(){
-        MapDirection dir = MapDirection.NORTH;
-        assertEquals(MapDirection.EAST, dir.next());
-    }
-    @Test
-    void NextForEastIsSouth(){
-        assertEquals(MapDirection.SOUTH, MapDirection.EAST.next());
-    }
-    @Test
-    void NextForSouthIsWest(){
-        assertEquals(MapDirection.WEST, MapDirection.SOUTH.next());
-    }
-    @Test
-    void NextForWestIsNorth(){
-        assertEquals(MapDirection.NORTH, MapDirection.WEST.next());
+    void NextDirectionTest(){
+        assertAll(
+                ()-> assertEquals(MapDirection.SOUTH, MapDirection.EAST.next()),
+                ()-> assertEquals(MapDirection.WEST, MapDirection.SOUTH.next()),
+                ()-> assertEquals(MapDirection.NORTH, MapDirection.WEST.next()),
+                ()-> assertEquals(MapDirection.EAST, MapDirection.NORTH.next())
+        );
     }
 
     @Test
-    void previousForNorthIsWest(){
-        assertEquals(MapDirection.WEST, MapDirection.NORTH.previous());
-    }
-    @Test
-    void previousForEastIsNorth(){
-        assertEquals(MapDirection.NORTH, MapDirection.EAST.previous());
-    }
-    @Test
-    void previousForSouthIsEast(){
-        assertEquals(MapDirection.EAST, MapDirection.SOUTH.previous());
-    }
-    @Test
-    void previousForWestIsSouth(){
-        assertEquals(MapDirection.SOUTH, MapDirection.WEST.previous());
+    void PreviousDirectionTest(){
+        assertAll(
+                ()-> assertEquals(MapDirection.NORTH, MapDirection.EAST.previous()),
+                ()-> assertEquals(MapDirection.EAST, MapDirection.SOUTH.previous()),
+                ()-> assertEquals(MapDirection.SOUTH, MapDirection.WEST.previous()),
+                ()-> assertEquals(MapDirection.WEST, MapDirection.NORTH.previous())
+        );
     }
 }
